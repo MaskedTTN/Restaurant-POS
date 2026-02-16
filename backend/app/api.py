9f91d41ec39717e05c085a19a65dc822a39918df
MaskedTTN <mustaeensiddiqui27@gmail.com>
2026-02-16 13:52:08 +0000

    Fixed CORS origins allow list

--- backend/app/api.py
+++ backend/app/api.py
@@ -25,7 +25,9 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "*",
+    "http://pos.wizard.cat",
+    "http://192.168.1.93:3000",
+    "http://192.168.1.93"
 ]
 
 app.add_middleware(
